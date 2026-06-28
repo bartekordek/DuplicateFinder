8378d1d3d8649bff5d543139e3259c110a210c2d
Bartłomiej Kordek <bartekordek@gmail.com>
2026-06-28 13:32:58 +0200

    Build fixes.

--- DuplicateFinderBase/inc/DuplicateFinderBase.hpp
+++ DuplicateFinderBase/inc/DuplicateFinderBase.hpp
@@ -1,28 +1,60 @@
 #pragma once
 
+#include "Import.hpp"
 #include "CUL/String/StringWrapper.hpp"
+#include "CUL/Filesystem/FileDatabase.hpp"
 #include "CUL/GenericUtils/NonCopyable.hpp"
 #include "CUL/STL_IMPORTS/STD_vector.hpp"
 #include "CUL/STL_IMPORTS/STD_mutex.hpp"
 
+namespace CUL
+{
+class CULInterface;
+}
+
+
+namespace CUL::FS
+{
+enum class ELockType : std::uint8_t;
+}
+
+using String = CUL::StringWr;
 using PathList = std::vector<CUL::StringWr>;
+using FileInfo = CUL::FS::FileInfo;
 
 class DuplicateFinderBase
 {
 public:
-    DuplicateFinderBase();
-    void addPath( const CUL::StringWr& inPath );
-    void removePath( const CUL::StringWr& inPath );
-    void removeLast();
+    DUP_FIN_BASE_API DuplicateFinderBase();
 
-    const PathList getPaths() const;
-    ~DuplicateFinderBase();
+    DUP_FIN_BASE_API void startDBLoad();
+    DUP_FIN_BASE_API String getModTimeFromDb( const String& filePath );
+    DUP_FIN_BASE_API void getListOfSizes( std::vector<uint64_t>& out ) const;
+    DUP_FIN_BASE_API void getFiles( uint64_t inSize, std::vector<FileInfo>& out ) const;
+    DUP_FIN_BASE_API void getFiles( uint64_t size, const CUL::StringWr& md5, std::vector<FileInfo>& out ) const;
+    DUP_FIN_BASE_API std::optional<FileInfo> getFileInfo_Impl( CUL::FS::ELockType inLockType, const CUL::StringWr& path ) const;
+    DUP_FIN_BASE_API void removeFileFromDB( const String& pathRaw );
+
+    DUP_FIN_BASE_API void addPath( const String& inPath );
+    DUP_FIN_BASE_API void removePath( const String& inPath );
+    DUP_FIN_BASE_API void removeLast();
+
+    DUP_FIN_BASE_API bool isLoadingDb() const;
+    DUP_FIN_BASE_API CUL::FS::CacheUsage getCacheUsage() const;
+
+    DUP_FIN_BASE_API const PathList getPaths() const;
+    DUP_FIN_BASE_API ~DuplicateFinderBase();
 
 protected:
 private:
-
+    CUL::CULInterface& m_interface;
     mutable std::mutex m_searchPathsMtx;
     PathList m_searchPaths;
+
+    std::atomic_bool m_loadingDb = false;
+    bool m_initialDbFilesUpdated = false;
+    std::unique_ptr<CUL::FS::FileDatabase> m_fileDb;
+
 public:
-    CUL_NONCOPYABLE(DuplicateFinderBase);
+    CUL_NONCOPYABLE( DuplicateFinderBase );
 };
--- DuplicateFinderBase/src/DuplicateFinderBase.cpp
+++ DuplicateFinderBase/src/DuplicateFinderBase.cpp
@@ -1,9 +1,21 @@
 #include "DuplicateFinderBase.hpp"
+#include "CUL/CULInterface.hpp"
+
+#include "CUL/Profiling/Profiler.hpp"
+#include "CUL/Threading/MultiWorkerSystem.hpp"
+#include "CUL/Threading/TaskCallback.hpp"
+#include "CUL/Threading/ThreadUtil.hpp"
 #include <CUL/STL_IMPORTS/STD_algorithm.hpp>
 
-DuplicateFinderBase::DuplicateFinderBase()
+const std::string BWorkerStatus( int8_t workerId, const std::string& status )
 {
+    return "[" + std::to_string( workerId ) + "] " + status;
+}
 
+DuplicateFinderBase::DuplicateFinderBase():
+    m_interface( *CUL::CULInterface::createInstance() )
+{
+    m_fileDb = std::make_unique<CUL::FS::FileDatabase>();
 }
 
 
@@ -35,6 +47,72 @@ const PathList DuplicateFinderBase::getPaths() const
 {
     std::lock_guard<std::mutex> locker( m_searchPathsMtx );
     return m_searchPaths;
+}
+
+void DuplicateFinderBase::startDBLoad()
+{
+    ProfilerScope( "CApp::searchBackground::startDBLoad" );
+    CUL::TaskCallback* loadDbTask = new CUL::TaskCallback();
+    loadDbTask->Callback = [this]( int8_t workerId )
+    {
+        ProfilerScope( "CApp::searchBackground::startDBLoad::callback" );
+        CUL::ThreadUtil::getInstance().setThreadStatus( BWorkerStatus( workerId, "loading data from db..." ) );
+        m_loadingDb = true;
+        m_fileDb->loadFilesFromDatabase();
+        m_loadingDb = false;
+        m_initialDbFilesUpdated = true;
+        CUL::ThreadUtil::getInstance().setThreadStatus( BWorkerStatus( workerId, "IDLE" ) );
+    };
+    loadDbTask->Type = CUL::ITask::EType::DeleteAfterExecute;
+    loadDbTask->Priority = CUL::EPriority::High;
+    CUL::MultiWorkerSystem::getInstance().registerTask( loadDbTask );
+}
+
+String DuplicateFinderBase::getModTimeFromDb( const String& filePath )
+{
+    ProfilerScope( "CApp::getModTimeFromDb" );
+    std::optional<CUL::FS::FileInfo> info = m_fileDb->getFileInfo( filePath.getValue() );
+
+    if( info->MD5.empty() == false )
+    {
+        CUL::Time currentTime;
+        CUL::FS::Path path;
+        path.createFrom( info->FilePath );
+        path.getLastModificationTime( currentTime );
+        return currentTime.cStr();
+    }
+
+    return "";
+}
+
+void DuplicateFinderBase::getListOfSizes( std::vector<uint64_t>& out ) const
+{
+    m_fileDb->getListOfSizes( out );
+}
+
+void DuplicateFinderBase::getFiles( uint64_t inSize, std::vector<FileInfo>& out ) const
+{
+    m_fileDb->getFiles( inSize, out );
+}
+
+void DuplicateFinderBase::getFiles( uint64_t size, const CUL::StringWr& md5, std::vector<FileInfo>& out ) const
+{
+    m_fileDb->getFiles( size, md5, out );
+}
+
+void DuplicateFinderBase::removeFileFromDB( const String& pathRaw )
+{
+    m_fileDb->removeFileFromDB( pathRaw );
+}
+
+bool DuplicateFinderBase::isLoadingDb() const
+{
+    return m_loadingDb;
+}
+
+CUL::FS::CacheUsage DuplicateFinderBase::getCacheUsage() const
+{
+    return m_fileDb->getCacheUsage();
 }
 
 DuplicateFinderBase::~DuplicateFinderBase()
